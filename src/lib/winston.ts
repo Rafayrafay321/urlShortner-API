46e450271deedccb548f9ce3e2df2d813829cc75
Rafayrafay321 <alihaidersh72@gmail.com>
2026-01-28 22:35:06 +0500

    Configured the logger for effective logging

--- src/lib/winston.ts
+++ src/lib/winston.ts
@@ -1,9 +1,38 @@
-// // Node modules
-// import { createLogger, transport, transports } from 'winston';
-// import { Logtail } from '@logtail/node';
-// import { LogtailTransport } from '@logtail/winston';
+// Node imports
+import { createLogger, transports, format } from 'winston';
 
-// // custom modules
-// import config from '@/config';
+// Custom imports
+import config from '@/config';
 
-// const transportation: t;
+const isProd = config.NODE_ENV === 'production';
+
+const logger = createLogger({
+  level: isProd ? 'info' : 'debug',
+  format: format.combine(
+    format.timestamp({
+      format: 'YYYY-MM-DD hh:mm:ss.SSS A',
+    }),
+    format.errors({ stack: true }),
+    format.align(),
+    format.json(),
+  ),
+  transports: [
+    new transports.File({
+      filename: 'logs/combined.log',
+    }),
+    new transports.File({
+      filename: 'logs/app-error.log',
+      level: 'error',
+    }),
+  ],
+});
+
+if (!isProd) {
+  logger.add(
+    new transports.Console({
+      format: format.combine(format.colorize({ all: true }), format.simple()),
+    }),
+  );
+}
+
+export default logger;
