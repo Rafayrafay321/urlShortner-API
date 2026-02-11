0f9283b2fe4769c4a6a6936b00c1f0f52cc61f01
Rafayrafay321 <alihaidersh72@gmail.com>
2026-02-11 23:59:10 +0500

    migrated register to postgres

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,48 +1,36 @@
 // Node modules
 import mongoose from 'mongoose';
-import type { HydratedDocument, Model, Types } from 'mongoose';
+import type { HydratedDocument, Model } from 'mongoose';
 
 // Custom Modules
 import logger from '@/lib/winston';
-import { IUser, User } from '@/models/user';
+import { prisma } from '@/config/prisma';
 import { Url } from '@/models/url';
 import { IUrl } from '@/models/url';
+import { Prisma } from '@prisma/client';
 
 // Types
 type UrlQuery = {
-  userId?: Types.ObjectId;
+  userId?: string;
   originalUrl?: string;
   shortUrl?: string;
 };
 
 type UserQuery = {
+  id?: string;
   email?: string;
-  userId?: Types.ObjectId;
-  refreshToken?: string;
 };
 
-// Function returning the whole document of user.
-export const userExists = async (
-  params: UserQuery,
-): Promise<HydratedDocument<IUser> | null> => {
-  if (mongoose.connection.readyState !== 1) {
-    logger.warn('Database not connected. Cannot check User existence.', {
-      params,
-    });
-    return null;
-  }
-  const query: UserQuery = {};
-  if (params.userId) {
-    query.userId = params.userId;
-  }
-  if (params.email) {
-    query.email = params.email;
-  }
-  if (params.refreshToken) {
-    query.refreshToken = params.refreshToken;
-  }
+// Check for existence of User.
+// TODO Do proper return type using ZOD
 
-  return User.findOne(query);
+export const userExists = async (params: UserQuery) => {
+  const condition: Prisma.UserWhereInput[] = [];
+
+  if (params.id) condition.push({ id: params.id });
+  if (params.email) condition.push({ email: params.email });
+  if (condition.length === 0) return null;
+  return prisma.user.findFirst({ where: { OR: condition } });
 };
 
 // Function returning whole document of the URL exisiting.
@@ -72,16 +60,13 @@ export const urlExists = async (
 };
 
 // Compare two object ids.
-export const compareIds = (
-  userId: Types.ObjectId,
-  urlId: Types.ObjectId,
-): boolean => {
-  return urlId.equals(userId);
+export const compareIds = (userId: string, urlId: string): boolean => {
+  return urlId === userId;
 };
 
 // Delete the document of provided Id.
 export const deleteDocument = async <T>(
-  id: Types.ObjectId,
+  id: string,
   model: Model<T>,
 ): Promise<HydratedDocument<T> | null> => {
   const deletedDoc = await model.findByIdAndDelete(id);
