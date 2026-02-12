be34e8eeece7068184a8369cdcf789f7ee8f239c
Rafayrafay321 <alihaidersh72@gmail.com>
2026-02-12 22:29:02 +0500

    migrated listingUrl to prisma

--- src/controllers/url/listUrls.ts
+++ src/controllers/url/listUrls.ts
@@ -1,12 +1,28 @@
-// Custom imports
-import { Url } from '@/models/url';
+// Custom import
+import { AppError } from '@/lib/appError';
+import { prisma } from '@/config/prisma';
 // Types
 import { Request, Response, NextFunction } from 'express';
 
 const listUrl = async (req: Request, res: Response, next: NextFunction) => {
   try {
-    const userId = req.userId;
-    const urlList = await Url.find({ userId });
+    if (!req.userId) {
+      throw new AppError(401, 'Unauthorized', 'Not allowed.');
+    }
+
+    const userId = req.userId.toString();
+    const urlList = await prisma.url.findMany({
+      where: {
+        userId: userId,
+      },
+      include: {
+        click: true,
+      },
+      orderBy: {
+        createdAt: 'desc',
+      },
+    });
+
     res.status(200).json({
       code: 'Success',
       urlList,
